be3713bb11492ed78ff3c4392a88a9a4473320bc
David Barrett <dabarre@microsoft.com>
2021-05-14 14:00:13 +0100

    Updated sample to access public folders and retrieve items.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,6 +41,7 @@ namespace EWSOAuthAppPermissions
             this.buttonClose = new System.Windows.Forms.Button();
             this.buttonFindFolders = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBoxGetPublicFolders = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBoxMailboxSMTPAddress = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
@@ -56,9 +57,11 @@ namespace EWSOAuthAppPermissions
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBoxTenantId);
             this.groupBox1.Controls.Add(this.textBoxAppId);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(18, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(327, 99);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(490, 152);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Application Information";
@@ -66,57 +69,65 @@ namespace EWSOAuthAppPermissions
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Location = new System.Drawing.Point(9, 114);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.Size = new System.Drawing.Size(104, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Client Secret:";
             // 
             // textBoxClientSecret
             // 
-            this.textBoxClientSecret.Location = new System.Drawing.Point(86, 71);
+            this.textBoxClientSecret.Location = new System.Drawing.Point(129, 109);
+            this.textBoxClientSecret.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxClientSecret.Name = "textBoxClientSecret";
-            this.textBoxClientSecret.Size = new System.Drawing.Size(235, 20);
+            this.textBoxClientSecret.Size = new System.Drawing.Size(350, 26);
             this.textBoxClientSecret.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Location = new System.Drawing.Point(9, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(81, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Tenant Id:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.Size = new System.Drawing.Size(109, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Application Id:";
             // 
             // textBoxTenantId
             // 
-            this.textBoxTenantId.Location = new System.Drawing.Point(86, 45);
+            this.textBoxTenantId.Location = new System.Drawing.Point(129, 69);
+            this.textBoxTenantId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxTenantId.Name = "textBoxTenantId";
-            this.textBoxTenantId.Size = new System.Drawing.Size(235, 20);
+            this.textBoxTenantId.Size = new System.Drawing.Size(350, 26);
             this.textBoxTenantId.TabIndex = 1;
             // 
             // textBoxAppId
             // 
-            this.textBoxAppId.Location = new System.Drawing.Point(86, 19);
+            this.textBoxAppId.Location = new System.Drawing.Point(129, 29);
+            this.textBoxAppId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxAppId.Name = "textBoxAppId";
-            this.textBoxAppId.Size = new System.Drawing.Size(235, 20);
+            this.textBoxAppId.Size = new System.Drawing.Size(350, 26);
             this.textBoxAppId.TabIndex = 0;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.textBoxResults);
-            this.groupBox2.Location = new System.Drawing.Point(12, 171);
+            this.groupBox2.Location = new System.Drawing.Point(18, 291);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(327, 111);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(490, 171);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Results";
@@ -124,19 +135,21 @@ namespace EWSOAuthAppPermissions
             // textBoxResults
             // 
             this.textBoxResults.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxResults.Location = new System.Drawing.Point(3, 16);
+            this.textBoxResults.Location = new System.Drawing.Point(4, 24);
+            this.textBoxResults.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxResults.Multiline = true;
             this.textBoxResults.Name = "textBoxResults";
             this.textBoxResults.ReadOnly = true;
             this.textBoxResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxResults.Size = new System.Drawing.Size(321, 92);
+            this.textBoxResults.Size = new System.Drawing.Size(482, 142);
             this.textBoxResults.TabIndex = 0;
             // 
             // buttonClose
             // 
-            this.buttonClose.Location = new System.Drawing.Point(264, 288);
+            this.buttonClose.Location = new System.Drawing.Point(396, 471);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.Size = new System.Drawing.Size(112, 35);
             this.buttonClose.TabIndex = 7;
             this.buttonClose.Text = "Close";
             this.buttonClose.UseVisualStyleBackColor = true;
@@ -144,9 +157,10 @@ namespace EWSOAuthAppPermissions
             // 
             // buttonFindFolders
             // 
-            this.buttonFindFolders.Location = new System.Drawing.Point(12, 285);
+            this.buttonFindFolders.Location = new System.Drawing.Point(18, 471);
+            this.buttonFindFolders.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonFindFolders.Name = "buttonFindFolders";
-            this.buttonFindFolders.Size = new System.Drawing.Size(75, 23);
+            this.buttonFindFolders.Size = new System.Drawing.Size(112, 35);
             this.buttonFindFolders.TabIndex = 6;
             this.buttonFindFolders.Text = "Find Folders";
             this.buttonFindFolders.UseVisualStyleBackColor = true;
@@ -154,43 +168,60 @@ namespace EWSOAuthAppPermissions
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.checkBoxGetPublicFolders);
             this.groupBox3.Controls.Add(this.label4);
             this.groupBox3.Controls.Add(this.textBoxMailboxSMTPAddress);
-            this.groupBox3.Location = new System.Drawing.Point(12, 117);
+            this.groupBox3.Location = new System.Drawing.Point(18, 180);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(327, 48);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Size = new System.Drawing.Size(490, 102);
             this.groupBox3.TabIndex = 8;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Mailbox";
             // 
+            // checkBoxGetPublicFolders
+            // 
+            this.checkBoxGetPublicFolders.AutoSize = true;
+            this.checkBoxGetPublicFolders.Checked = true;
+            this.checkBoxGetPublicFolders.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxGetPublicFolders.Location = new System.Drawing.Point(129, 69);
+            this.checkBoxGetPublicFolders.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBoxGetPublicFolders.Name = "checkBoxGetPublicFolders";
+            this.checkBoxGetPublicFolders.Size = new System.Drawing.Size(191, 24);
+            this.checkBoxGetPublicFolders.TabIndex = 8;
+            this.checkBoxGetPublicFolders.Text = "Retrieve public folders";
+            this.checkBoxGetPublicFolders.UseVisualStyleBackColor = true;
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 22);
+            this.label4.Location = new System.Drawing.Point(9, 34);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.Size = new System.Drawing.Size(117, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "SMTP address:";
             // 
             // textBoxMailboxSMTPAddress
             // 
-            this.textBoxMailboxSMTPAddress.Location = new System.Drawing.Point(86, 19);
+            this.textBoxMailboxSMTPAddress.Location = new System.Drawing.Point(129, 29);
+            this.textBoxMailboxSMTPAddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxMailboxSMTPAddress.Name = "textBoxMailboxSMTPAddress";
-            this.textBoxMailboxSMTPAddress.Size = new System.Drawing.Size(235, 20);
+            this.textBoxMailboxSMTPAddress.Size = new System.Drawing.Size(350, 26);
             this.textBoxMailboxSMTPAddress.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(355, 319);
+            this.ClientSize = new System.Drawing.Size(528, 520);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.buttonClose);
             this.Controls.Add(this.buttonFindFolders);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -221,6 +252,7 @@ namespace EWSOAuthAppPermissions
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBoxMailboxSMTPAddress;
+        private System.Windows.Forms.CheckBox checkBoxGetPublicFolders;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Xml;
+using System.Net.Http;
 using System.Windows.Forms;
 using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Exchange.WebServices.Autodiscover;
 using Microsoft.Identity.Client;
 
 namespace EWSOAuthAppPermissions
 {
     public partial class Form1 : Form
     {
+        HttpClient _httpClient = new HttpClient();
+
         public Form1()
         {
             InitializeComponent();
@@ -46,21 +51,37 @@ namespace EWSOAuthAppPermissions
                         .ExecuteAsync();
 
                     // Configure the ExchangeService with the access token
-                    var ewsClient = new ExchangeService();
+                    var ewsClient = new ExchangeService(ExchangeVersion.Exchange2016);
                     ewsClient.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
                     ewsClient.Credentials = new OAuthCredentials(result.AccessToken);
+                    ewsClient.TraceListener = new TraceListener("trace.log");
+                    ewsClient.TraceFlags = TraceFlags.All;
+                    ewsClient.TraceEnabled = true;
+
+                    _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {result.AccessToken}");
 
                     //Impersonate the mailbox you'd like to access.
                     ewsClient.ImpersonatedUserId = new ImpersonatedUserId(ConnectingIdType.SmtpAddress, textBoxMailboxSMTPAddress.Text);
 
-                    //Include x-anchormailbox header
-                    ewsClient.HttpHeaders.Add("X-AnchorMailbox", textBoxMailboxSMTPAddress.Text);
-
                     // Make an EWS call
-                    var folders = ewsClient.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
-                    foreach (var folder in folders)
+                    if (checkBoxGetPublicFolders.Checked)
                     {
-                        WriteToResults($"Folder: {folder.DisplayName}");
+                        SetPublicFolderHeaders(ewsClient, textBoxMailboxSMTPAddress.Text);
+                        var folders = ewsClient.FindFolders(WellKnownFolderName.PublicFoldersRoot, new FolderView(10));
+                        foreach (var folder in folders)
+                        {
+                            WriteToResults($"Folder: {folder.DisplayName}");
+                            ReadItemsFromFolder(folder);
+                        }
+                    }
+                    else
+                    {
+                        ewsClient.HttpHeaders.Add("X-AnchorMailbox", textBoxMailboxSMTPAddress.Text);
+                        var folders = ewsClient.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
+                        foreach (var folder in folders)
+                        {
+                            WriteToResults($"Folder: {folder.DisplayName}");
+                        }
                     }
                 }
                 catch (MsalException ex)
@@ -82,6 +103,63 @@ namespace EWSOAuthAppPermissions
             Close();
         }
 
+        private void SetPublicFolderHeaders(ExchangeService exchangeService, string mailbox)
+        {
+            // For public folders, we first need to get X-AnchorMailbox
 
+            // Perform autodiscover for the mailbox and store the information
+            AutodiscoverService autodiscover = new AutodiscoverService(new Uri("https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc"), ExchangeVersion.Exchange2016);
+            autodiscover.Credentials = exchangeService.Credentials;
+            autodiscover.TraceListener = exchangeService.TraceListener;
+            autodiscover.TraceFlags = TraceFlags.All;
+            autodiscover.TraceEnabled = true;
+
+            // Retrieve the autodiscover information
+            GetUserSettingsResponse userSettings = null;
+            try
+            {
+                userSettings = autodiscover.GetUserSettings(mailbox, UserSettingName.PublicFolderInformation);
+            }
+            catch (Exception ex)
+            {
+                WriteToResults($"Error: {ex}");
+                return;
+            }
+            string xAnchor = userSettings.Settings[UserSettingName.PublicFolderInformation].ToString();
+            WriteToResults($"Set X-AnchorMailbox to {xAnchor}");
+            exchangeService.HttpHeaders.Add("X-AnchorMailbox", xAnchor);
+
+
+            // Per the Exchange 2003 docs, we need to do a further AutoDiscover request to get X-PublicFolderMailbox
+            // For Office 365, this will return an email address not found.
+            // It doesn't seem to be required to set this header for Office 365, access to the public folder succeeds without it.
+
+            /*
+            string autodiscoverXml = System.IO.File.ReadAllText("Autodiscover.xml").Replace("<!--EMAILADDRESS-->", xAnchor);
+            
+            HttpResponseMessage response = _httpClient.PostAsync("https://autodiscover-s.outlook.com/autodiscover/autodiscover.xml",
+                new StringContent(autodiscoverXml, System.Text.Encoding.UTF8, "text/xml")).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                WriteToResults("Failed to get X-PublicFolderMailbox");
+                return;
+            }
+
+            string autodiscoverResponse = response.Content.ReadAsStringAsync().Result;
+            XmlDocument autodiscoverXmlResponse = new XmlDocument();
+            autodiscoverXmlResponse.LoadXml(autodiscoverResponse);
+
+            WriteToResults(autodiscoverXmlResponse.ToString());
+            //WriteToResults($"Set X-PublicFolderMailbox to {userSettings.Settings[UserSettingName.ExternalMailboxServer]}");
+            */
+        }
+
+        private void ReadItemsFromFolder(Folder folder)
+        {
+            ItemView itemView = new ItemView(10, 0, OffsetBasePoint.Beginning);
+            FindItemsResults<Item> results = folder.FindItems(itemView);
+            foreach (var item in results.Items)
+                WriteToResults($"Item: {item.Subject}");
+        }
     }
 }
