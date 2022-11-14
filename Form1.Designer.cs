11002495b3c6cb99fa1934871827c0f087a50f52
David Barrett <dabarre@microsoft.com>
2022-11-14 13:57:25 +0000

    Removed app info.  Default to NO public folders.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -61,9 +61,11 @@ namespace EWSOAuthAppPermissions
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBoxTenantId);
             this.groupBox1.Controls.Add(this.textBoxAppId);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(18, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(327, 99);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(490, 152);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Application Information";
@@ -71,60 +73,65 @@ namespace EWSOAuthAppPermissions
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Location = new System.Drawing.Point(9, 114);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.Size = new System.Drawing.Size(104, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Client Secret:";
             // 
             // textBoxClientSecret
             // 
-            this.textBoxClientSecret.Location = new System.Drawing.Point(86, 71);
+            this.textBoxClientSecret.Location = new System.Drawing.Point(129, 109);
+            this.textBoxClientSecret.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxClientSecret.Name = "textBoxClientSecret";
-            this.textBoxClientSecret.Size = new System.Drawing.Size(235, 20);
+            this.textBoxClientSecret.Size = new System.Drawing.Size(350, 26);
             this.textBoxClientSecret.TabIndex = 4;
-            this.textBoxClientSecret.Text = "PSQ8Q~3yNjJIAJw5OSsFQbB8UaWS5HDGPsdUIcJ9";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Location = new System.Drawing.Point(9, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(81, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Tenant Id:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.Size = new System.Drawing.Size(109, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Application Id:";
             // 
             // textBoxTenantId
             // 
-            this.textBoxTenantId.Location = new System.Drawing.Point(86, 45);
+            this.textBoxTenantId.Location = new System.Drawing.Point(129, 69);
+            this.textBoxTenantId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxTenantId.Name = "textBoxTenantId";
-            this.textBoxTenantId.Size = new System.Drawing.Size(235, 20);
+            this.textBoxTenantId.Size = new System.Drawing.Size(350, 26);
             this.textBoxTenantId.TabIndex = 1;
-            this.textBoxTenantId.Text = "fc69f6a8-90cd-4047-977d-0c768925b8ec";
             // 
             // textBoxAppId
             // 
-            this.textBoxAppId.Location = new System.Drawing.Point(86, 19);
+            this.textBoxAppId.Location = new System.Drawing.Point(129, 29);
+            this.textBoxAppId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxAppId.Name = "textBoxAppId";
-            this.textBoxAppId.Size = new System.Drawing.Size(235, 20);
+            this.textBoxAppId.Size = new System.Drawing.Size(350, 26);
             this.textBoxAppId.TabIndex = 0;
-            this.textBoxAppId.Text = "8e4e9630-3722-4a57-9b75-412e05410ef7";
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.textBoxResults);
-            this.groupBox2.Location = new System.Drawing.Point(12, 215);
+            this.groupBox2.Location = new System.Drawing.Point(18, 331);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(327, 125);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(490, 192);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Results";
@@ -132,19 +139,21 @@ namespace EWSOAuthAppPermissions
             // textBoxResults
             // 
             this.textBoxResults.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxResults.Location = new System.Drawing.Point(3, 16);
+            this.textBoxResults.Location = new System.Drawing.Point(4, 24);
+            this.textBoxResults.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxResults.Multiline = true;
             this.textBoxResults.Name = "textBoxResults";
             this.textBoxResults.ReadOnly = true;
             this.textBoxResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxResults.Size = new System.Drawing.Size(321, 106);
+            this.textBoxResults.Size = new System.Drawing.Size(482, 163);
             this.textBoxResults.TabIndex = 0;
             // 
             // buttonClose
             // 
-            this.buttonClose.Location = new System.Drawing.Point(264, 346);
+            this.buttonClose.Location = new System.Drawing.Point(396, 532);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.Size = new System.Drawing.Size(112, 35);
             this.buttonClose.TabIndex = 7;
             this.buttonClose.Text = "Close";
             this.buttonClose.UseVisualStyleBackColor = true;
@@ -152,9 +161,10 @@ namespace EWSOAuthAppPermissions
             // 
             // buttonFindFolders
             // 
-            this.buttonFindFolders.Location = new System.Drawing.Point(12, 347);
+            this.buttonFindFolders.Location = new System.Drawing.Point(18, 534);
+            this.buttonFindFolders.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonFindFolders.Name = "buttonFindFolders";
-            this.buttonFindFolders.Size = new System.Drawing.Size(75, 23);
+            this.buttonFindFolders.Size = new System.Drawing.Size(112, 35);
             this.buttonFindFolders.TabIndex = 6;
             this.buttonFindFolders.Text = "Find Folders";
             this.buttonFindFolders.UseVisualStyleBackColor = true;
@@ -167,9 +177,11 @@ namespace EWSOAuthAppPermissions
             this.groupBox3.Controls.Add(this.checkBoxGetPublicFolders);
             this.groupBox3.Controls.Add(this.label4);
             this.groupBox3.Controls.Add(this.textBoxMailboxSMTPAddress);
-            this.groupBox3.Location = new System.Drawing.Point(12, 117);
+            this.groupBox3.Location = new System.Drawing.Point(18, 180);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(327, 92);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Size = new System.Drawing.Size(490, 142);
             this.groupBox3.TabIndex = 8;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Mailbox";
@@ -177,10 +189,9 @@ namespace EWSOAuthAppPermissions
             // checkBoxPOXBasicAuth
             // 
             this.checkBoxPOXBasicAuth.AutoSize = true;
-            this.checkBoxPOXBasicAuth.Location = new System.Drawing.Point(51, 66);
-            this.checkBoxPOXBasicAuth.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBoxPOXBasicAuth.Location = new System.Drawing.Point(76, 102);
             this.checkBoxPOXBasicAuth.Name = "checkBoxPOXBasicAuth";
-            this.checkBoxPOXBasicAuth.Size = new System.Drawing.Size(137, 17);
+            this.checkBoxPOXBasicAuth.Size = new System.Drawing.Size(201, 24);
             this.checkBoxPOXBasicAuth.TabIndex = 10;
             this.checkBoxPOXBasicAuth.Text = "Use basic auth for POX";
             this.toolTip1.SetToolTip(this.checkBoxPOXBasicAuth, "If selected, POX is used for AutoDiscover to determine X-PublicFolderMailbox (oth" +
@@ -189,10 +200,9 @@ namespace EWSOAuthAppPermissions
             // 
             // textBoxAutoDiscoverPW
             // 
-            this.textBoxAutoDiscoverPW.Location = new System.Drawing.Point(192, 63);
-            this.textBoxAutoDiscoverPW.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxAutoDiscoverPW.Location = new System.Drawing.Point(288, 97);
             this.textBoxAutoDiscoverPW.Name = "textBoxAutoDiscoverPW";
-            this.textBoxAutoDiscoverPW.Size = new System.Drawing.Size(129, 20);
+            this.textBoxAutoDiscoverPW.Size = new System.Drawing.Size(192, 26);
             this.textBoxAutoDiscoverPW.TabIndex = 9;
             this.toolTip1.SetToolTip(this.textBoxAutoDiscoverPW, "Public folder access requires information that can only be\r\nobtained via AutoDisc" +
         "over using basic auth.  Enter the\r\npassword for the mailbox here to ensure publi" +
@@ -202,11 +212,10 @@ namespace EWSOAuthAppPermissions
             // checkBoxGetPublicFolders
             // 
             this.checkBoxGetPublicFolders.AutoSize = true;
-            this.checkBoxGetPublicFolders.Checked = true;
-            this.checkBoxGetPublicFolders.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxGetPublicFolders.Location = new System.Drawing.Point(51, 45);
+            this.checkBoxGetPublicFolders.Location = new System.Drawing.Point(76, 69);
+            this.checkBoxGetPublicFolders.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBoxGetPublicFolders.Name = "checkBoxGetPublicFolders";
-            this.checkBoxGetPublicFolders.Size = new System.Drawing.Size(131, 17);
+            this.checkBoxGetPublicFolders.Size = new System.Drawing.Size(191, 24);
             this.checkBoxGetPublicFolders.TabIndex = 8;
             this.checkBoxGetPublicFolders.Text = "Retrieve public folders";
             this.toolTip1.SetToolTip(this.checkBoxGetPublicFolders, "If checked, public folders will be accessed instead of\r\nthe user\'s primary mailbo" +
@@ -217,33 +226,33 @@ namespace EWSOAuthAppPermissions
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 22);
+            this.label4.Location = new System.Drawing.Point(9, 34);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.Size = new System.Drawing.Size(117, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "SMTP address:";
             // 
             // textBoxMailboxSMTPAddress
             // 
-            this.textBoxMailboxSMTPAddress.Location = new System.Drawing.Point(86, 19);
+            this.textBoxMailboxSMTPAddress.Location = new System.Drawing.Point(129, 29);
+            this.textBoxMailboxSMTPAddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxMailboxSMTPAddress.Name = "textBoxMailboxSMTPAddress";
-            this.textBoxMailboxSMTPAddress.Size = new System.Drawing.Size(235, 20);
+            this.textBoxMailboxSMTPAddress.Size = new System.Drawing.Size(350, 26);
             this.textBoxMailboxSMTPAddress.TabIndex = 6;
-            this.textBoxMailboxSMTPAddress.Text = "dave@demonmaths.co.uk";
             this.toolTip1.SetToolTip(this.textBoxMailboxSMTPAddress, "The SMTP address of the mailbox being impersonated/accessed.");
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(349, 379);
+            this.ClientSize = new System.Drawing.Size(524, 583);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.buttonClose);
             this.Controls.Add(this.buttonFindFolders);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
